73babb5b149cd3786db5fedacd867d2b37009eb9
Jonas Arvén Norling <jonas.arven.norling@gmail.com>
2013-04-15 16:06:46 +0100

    Added table view controller

---
+++ JoJoJoTravels/JOTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JOTableViewController.h
+//  JoJoJoTravels
+//
+//  Created by Jonas Arvén on 4/15/13.
+//  Copyright (c) 2013 Johan Nenzén. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOTableViewController : UITableViewController
+
+@end
